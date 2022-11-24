05ec315e54b565431ade1cf160088c49397269a7
InfinitySwordofDiamond <63327437+InfinitySwordofDiamond@users.noreply.github.com>
2022-11-24 15:10:16 -0800

    Console won't spam
    Made it where when someone find the number, the console will not spam

--- detectnumbers.py
+++ detectnumbers.py
@@ -1,10 +1,19 @@
-from count import reviewMessages, detectMessages, textFileName
+from count import reviewMessages, request, textFileName, channelID, botID
+import time
 import os
 
 try:
     with open(os.path.dirname(__file__) + f"\\{textFileName}", "x") as file: file.write("")
 
 except: pass
+
+def checkMessages():
+    messageInfo = request("get", channelID, None, None, None)
+    count = 0
+
+    while count < 4:
+        if ("Winning Message" in messageInfo[count]['content'] and f"{botID}" in str(messageInfo[count]['author'])):                    # <--- Change
+            return 1
 
 removedNumber = None
 
@@ -17,7 +26,8 @@ while True:
     numbersUsed, dummyList, removedNumber = reviewMessages(numbersUsed, dummyList, 100, removedNumber)
     with open(os.path.dirname(__file__) + f"\\{textFileName}", "w") as file: file.write(str(numbersUsed))
 
-    if (detectMessages() == 1):
+    if (checkMessages() == 1):
         open(os.path.dirname(__file__) + f"\\{textFileName}", "w").close()
         print("\nThe number has been found so " + textFileName + "has been cleared\n")
         numbersUsed = []
+        time.sleep(20)
